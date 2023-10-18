55209ff59746eec5ff4326366765dafec1ca8a93
KOVUCZ <Hrdy.Adam@stud.vassboskovice.cz>
2023-10-18 12:52:12 +0200

    Tak to překopávám no, už to není nejlepší den :c

---
+++ Pepik_starne_Hrdy/osoba.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pepik_starne_Hrdy
+{
+    internal class osoba
+    {
+        public int Age { get => vek; }
+        public int vek;
+    }
+}
